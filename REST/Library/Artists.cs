63093b05b1eb75fb82223b44776a8f9bfd0063fd
justinzsy <justinzsy@gmail.com>
2018-12-21 14:36:42 -0600

    added some very basic classes in the Library for Albums, Artists, Songs, and Users; needs to be updated with final DB implementations

---
+++ REST/Library/Artists.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Library
+{
+    class Artists
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        // referece to a Locations table possibly?
+
+        public DateTime Formed { get; set; } // again, we only need the Date, not the Time of when the band was formed
+
+        // LatestRelease will pull an artist's latest album through the Album table
+        // need an AlbumID, or name assigned LatestRelease, waiting on DB finalization
+
+        public string Website { get; set; } // string holding URL to artist's Website (if it exists)
+    }
+}

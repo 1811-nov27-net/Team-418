cea2fdd547318d67e1b408102ef9c5c401b6ef83
John Pot <j.pot@digipen.edu>
2018-12-21 13:16:34 -0600

    Added initial ASP.NET Identity for user roles and logging in

---
+++ REST/WebApplication/Controllers/AccountController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    // Using ControllerBase instead of Controller since 
+    //  we don't need a view here.
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private SignInManager<IdentityUser> _signInManager { get; }
+
+        public AccountController(SignInManager<IdentityUser> signInManager, IdentityDbContext db)
+        {
+            // Create the authentication server if it does not
+            //  exist.
+            db.Database.EnsureCreated();
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(AccountViewModel account)
+        {
+            // Using the authorization database, attempt to sign in using signinmanager
+            // Persist = true will set the cookie to keep the user logged in for 
+            //  however long startup set the expiretimespan option for
+            var result = await _signInManager.PasswordSignInAsync(account.UserName, account.Password,
+                isPersistent: true, lockoutOnFailure: false);
+
+            if (!result.Succeeded)
+            {
+                return StatusCode(403); // Forbidden, invalid login
+            }
+
+            // User was logged in, nothing to return
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(
+           AccountViewModel account,
+           [FromServices] UserManager<IdentityUser> userManager,
+           [FromServices] RoleManager<IdentityRole> roleManager,
+           bool isAdmin = false)
+        {
+            if (ModelState.IsValid)
+            {
+                // Creating the new user 
+                var user = new IdentityUser(account.UserName);
+                // Wait for database to be updated
+                var userCreationResult = await userManager.CreateAsync(user, account.Password);
+
+                if (!userCreationResult.Succeeded)
+                {
+                    return BadRequest(userCreationResult);
+                }
+
+                // Set up user roles
+                if (isAdmin)
+                {
+                    // Create the admin role if it does not exist
+                    bool adminRoleExists = await roleManager.RoleExistsAsync("Admin");
+                    if (!adminRoleExists)
+                    {
+                        var adminRole = new IdentityRole("Admin");
+                        var adminCreationResult = await roleManager.CreateAsync(adminRole);
+                        if (!adminCreationResult.Succeeded)
+                        {
+                            // Server error
+                            return StatusCode(500, adminCreationResult);
+                        }
+
+                    }
+
+                    // Add the admin role to the user
+                    var userRoleSetResult = await userManager.AddToRoleAsync(user, "Admin");
+                    if (!userRoleSetResult.Succeeded)
+                    {
+                        // Server error
+                        return StatusCode(500, userRoleSetResult);
+                    }
+                }
+
+                // Sign the newly created user in
+                await _signInManager.SignInAsync(user, true);
+                return NoContent();
+
+            }
+            // Model state is invalid
+            else
+            {
+                // Generic client error 400
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return NoContent();
+        }
+    }
+}

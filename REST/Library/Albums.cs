9b2490dbb1808d3bc1606421f659e0c98062b1bc
justinzsy <justinzsy@gmail.com>
2018-12-31 16:17:07 -0600

    edited Library class files to match DB, and created PendingRequests.cs and AlbumSongs.cs

--- REST/Library/Albums.cs
+++ REST/Library/Albums.cs
@@ -5,17 +5,18 @@ using System.Text;
 
 namespace Library
 {
-    class Albums
+    public class Albums
     {
+        // Album Id set in DB
         public int Id { get; set; }
-
-        [Required]
+        // Name of album
         public string Name { get; set; }
-
-        public int ArtistId { get; set; }
-
+        // Id of Artist (foreign key reference)
+        public int Artist { get; set; }
+        // Date of album release, set as
+        // new DateTime(int year, int day, int month)
         public DateTime Release { get; set; }
-
+        // Genre of album
         public string Genre { get; set; }
     }
 }
